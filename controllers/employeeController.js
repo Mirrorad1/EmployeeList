89d4d8ac90e00ad52f50058e39d596e857cfefa7
Mirrorad1 <mirro.radosavlevici@gmail.com>
2018-12-22 23:45:45 -0700

    view is called and checks if input has been submitted

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -2,7 +2,11 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/',(req,res)=>{
-    res.json('test');
+    res.render("employee/editOrAdd", {
+        viewTitle: "Insert Employee"
+    });
 });
-
+router.post('/',(req,res)=>{
+    console.log("Input Success!");
+});
 module.exports = router;
