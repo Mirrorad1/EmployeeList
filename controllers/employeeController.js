beaa490b00f482a47101764bacea8c1fe6981f75
Mirrorad1 <mirro.radosavlevici@gmail.com>
2018-12-23 14:15:28 -0700

    More error handling done for form

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
 
 router.get('/',(req,res)=>{
     res.render("employee/editOrAdd", {
@@ -7,6 +9,48 @@ router.get('/',(req,res)=>{
     });
 });
 router.post('/',(req,res)=>{
-    console.log("Input Success!");
+     insertData(req,res); 
 });
+
+function insertData(req,res){
+    var employee = new Employee();
+    employee.fullName = req.body.fullName;
+    employee.email = req.body.email;
+    employee.mobile = req.body.mobile;
+    employee.city = req.body.city;
+    employee.save((err, doc) => {
+        if (!err)
+            res.redirect('employee/list');
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("employee/editOrAdd", {
+                    viewTitle: "Insert Employee",
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Error during record insertion : ' + err);
+        }
+    });
+}
+
+router.get('/list', (req, res) => {
+    res.json('from list');
+});
+function handleValidationError(err, body) {
+    for (field in err.errors) {
+        switch (err.errors[field].path) {
+            case 'fullName':
+                body['fullNameError'] = err.errors[field].message;
+                break;
+            case 'email':
+                body['emailError'] = err.errors[field].message;
+                break;
+            default:
+                break;
+        }
+    }
+}
+
 module.exports = router;
